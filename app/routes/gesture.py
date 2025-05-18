79aa3f514821cbc4f7cc7a193a97dee64ce1274b
이충인 <hooin0318@gmail.com>
2025-05-18 23:56:14 +0900

    모드 선택 및 해제 추가

--- app/routes/gesture.py
+++ app/routes/gesture.py
@@ -17,15 +17,35 @@ def handle_gesture():
     gesture = request.get_json().get("gesture")
     if not gesture:
         return jsonify({"error": "제스처 값이 없습니다."}), 400
+    
+    mode_data = db.reference(f"mode_gesture/{gesture}").get()
+    selected_mode = mode_data.get("mode") if mode_data else None
+
+    # 모드 설정
+    if selected_mode:
+        # 모드 선택
+        if not current_mode["value"]:
+            current_mode["value"] = selected_mode
+            return jsonify({"message": f"모드 '{selected_mode}'로 설정되었습니다."})
+        # 모드 해제
+        elif current_mode["value"] == selected_mode:
+            current_mode["value"] = None
+            return jsonify({"message": f"모드 '{selected_mode}'가 해제되었습니다."})
+        # 모드 전환
+        else:
+            prev = current_mode["value"]
+            current_mode["value"] = selected_mode
+            return jsonify({"message": f"모드 '{prev}'->'{selected_mode}'로 전환되었습니다."})
+
     if not current_mode["value"]:
         return jsonify({"error": "현재 모드가 설정되어 있지 않습니다."}), 400
-    
+
     mode = current_mode["value"]
     control_data = db.reference(f"control_gesture/{mode}/{gesture}").get()
     if control_data is None:
         return jsonify({"error": f"모드 '{mode}'에 제스처 '{gesture}'가 없습니다."}), 404
     
-    control = control_data.get("contorl")
+    control = control_data.get("control")
     ir_data = db.reference(f"ir_codes/{mode}/{control}").get()
     if not ir_data or not ir_data.get("code"):
         return jsonify({"error": "IR 코드가 없습니다."}), 404
